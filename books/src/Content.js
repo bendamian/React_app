56946072ba61fddb435c15cb0dfb8c8d3c654f7c
bendamian <badamian@gmail.com>
2022-11-21 18:22:43 +0000

    testing  useState Hook

--- books/src/Content.js
+++ books/src/Content.js
@@ -1,31 +1,68 @@
-import React from "react";
+//import React from "react";
+import { useState } from "react";
+import { FaTrashAlt } from "react-icons/fa";
 
 function Content() {
-  let memberNamme = () => {
-    let name = ["Tomy", "Ben", "Anton"];
-    let int = Math.floor(Math.random() * 3);
-    console.log(int);
-    return name[int];
+  const [books, setBook] = useState([
+    {
+      id: 1,
+      checked: true,
+      book: "JavaScript and JQuery"
+    },
+    {
+      id: 2,
+      checked: false,
+      book: "The Principles of Object-Oriented JavaScript"
+    },
+    {
+      id: 3,
+      checked: false,
+      book: "JavaScript from Beginner to Professional"
+    }
+  ]);
+
+  const handleCheched = id => {
+    const bookList = books.map(book =>
+      book.id === id ? { ...book, checked: !book.checked } : book
+    );
+    setBook(bookList);
+    localStorage.setItem("bookList", JSON.stringify(bookList));
   };
 
-  const handleClick = () => {
-    console.log("Add Book");
-  };
-
-  const handleClick2 = name => {
-    console.log(`${name} Clicked`);
-  };
-
-  const handleClick3 = e => {
-    console.log(e.target);
+  const handleDelete = id => {
+    const bookList = books.filter(book => book.id !== id);
+    setBook(bookList);
+    localStorage.setItem("bookList", JSON.stringify(bookList));
   };
 
   return (
     <main>
-      <p onDoubleClick={handleClick}>Hello {memberNamme()}</p>
-      <button onClick={handleClick}>Add Book</button>
-      <button onClick={() => handleClick2("Ben")}>Add Name</button>
-      <button onClick={e => handleClick3(e)}>Event Clicked</button>
+      {books.length ? (
+        <ul>
+          {books.map(book => (
+            <li className="book" key={book.id}>
+              <input
+                type="checkbox"
+                onChange={() => handleCheched(book.id)}
+                checked={book.checked}
+              ></input>
+              <label
+                style={book.checked ? { textDecoration: "line-through" } : null}
+                onDoubleClick={() => handleCheched(book.id)}
+              >
+                {book.book}
+              </label>
+              <FaTrashAlt
+                onClick={() => handleDelete(book.id)}
+                role="button fa-solid"
+                tabIndex="0"
+              />
+            </li>
+          ))}
+        </ul>
+      ) : (
+        <p style={{ marginTop: "2rem" }}> Empty Book List.</p>
+      )}
     </main>
   );
 }
