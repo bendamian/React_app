8ca3c7cd360a8da20cdc4a30443cf7960cf36a2c
bendamian <badamian@gmail.com>
2022-12-03 22:13:19 +0000

    testing useEffect Hook

--- books/src/App.js
+++ books/src/App.js
@@ -1,44 +1,47 @@
 import Header from "./Header";
 import Content from "./Content";
 import Footer from "./Footer";
-import { useState } from "react";
+import { useState ,useEffect} from "react";
 import AddBook from "./AddBook";
 import SearchBook from "./SearchBook";
 
 function App() {
-  const [books, setBook] = useState(JSON.parse(localStorage.getItem('bookList')));
+  const [books, setBook] = useState(
+    JSON.parse(localStorage.getItem("bookList")) || []
+  );
 
   const [newBook, setNewBook] = useState("");
   const [search,setSearch] = useState('')
 
-  const setAndSaveBook = newBooks => {
-    setBook(newBooks);
-    localStorage.setItem("bookList", JSON.stringify(newBooks));
-  };
+  useEffect(()=>{
+localStorage.setItem('bookList', JSON.stringify(books));
+  },[books])
+
+
 
   const addBook = book => {
     const id = books.length ? books[books.length - 1].id + 1 : 1;
     const myNewBook = { id, checked: false, book };
     const bookList = [...books, myNewBook];
-    setAndSaveBook(bookList);
+    setBook(bookList);
   };
 
   const handleCheched = id => {
     const bookList = books.map(book =>
       book.id === id ? { ...book, checked: !book.checked } : book
     );
-    setAndSaveBook(bookList);
+    setBook(bookList);
   };
 
   const handleDelete = id => {
     const bookList = books.filter(book => book.id !== id);
-    setAndSaveBook(bookList);
+    setBook(bookList);
   };
 
   const handleSubmit = e => {
     e.preventDefault();
     if (!newBook) return;
-    console.log(newBook);
+    //console.log(newBook);
     // addBook
     addBook(newBook)
     setNewBook("");
